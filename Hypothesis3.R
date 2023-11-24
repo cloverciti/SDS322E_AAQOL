89a8c0ea0f6a4faf1018382ac400e178df18d7d9
ValeriKhai <111999814+ValeriKhai@users.noreply.github.com>
2023-11-24 01:38:11 -0600

    Update Hypothesis3.R

--- Hypothesis3.R
+++ Hypothesis3.R
@@ -1,3 +1,5 @@
+#V1
+
 IncomeBasedOnReligion <- AAQoL %>% 
   select(Income, Religion) %>% 
   na.omit() %>% 
@@ -13,6 +15,22 @@ IncomeBasedOnReligionFinal <- merge(IncomeBasedOnReligion, totalresp, by = "Reli
 
 # Plot using ggplot
 ggplot(IncomeBasedOnReligionFinal, aes(x = Religion, y = percentage, fill = Income)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Percentage of People in Each Religion Group by Income",
+       x = "Income",
+       y = "Percentage") +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  theme_minimal()
+
+#V2
+
+IncomeBasedOnReligionFinal1 <- IncomeBasedOnReligionFinal
+
+Income_level <- c("$70,000 and over", "$60,000 - $69,999", "$50,000 - $59,999", "$40,000 - $49,999", "$30,000 - $39,999", "$20,000 - $29,999", "$10,000 - $19,999", "$0 - $9,999")
+
+IncomeBasedOnReligionFinal1 <- IncomeBasedOnReligionFinal1 %>% mutate(Income = factor(Income, levels = Income_level))
+
+ggplot(IncomeBasedOnReligionFinal1, aes(x = Religion, y = percentage, fill = Income)) +
   geom_bar(stat = "identity") +
   labs(title = "Percentage of People in Each Religion Group by Income",
        x = "Income",
